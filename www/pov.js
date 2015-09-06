65818fbc3bcd49c4bb98de8df408e4032e4800c3
peccadille <joh-peccadille@hotmail.fr>
2015-09-06 17:54:08 +0200

    correction du bug sur l'affichage des tableaux sélectionnés, réintialisation de la carte à chaque envoi du formulaire

--- www/pov.js
+++ www/pov.js
@@ -8,103 +8,123 @@ var MarkerIcon2 = L.icon({
 	iconAnchor: [16, 30],
 	popupAnchor: [0, -32]
 });
+var markers_layer = new L.FeatureGroup();
+map.addLayer(markers_layer);
 
+function sendAjaxForm(formulaire){
+	var postData = formulaire.serializeArray();
+	var formURL = formulaire.attr("action");
+	$.ajax(
+	{
+			url : formURL,
+			type: "POST",
+			data : postData,
+			success:function(data, textStatus, jqXHR)
+			{
+		//Je déclare le layer qui va recevoir les markers
+		markers_layer.clearLayers();
+		/*Je boucle sur le tableau */
+		for (var i = 0; i < data.length ; i++)
+		{
+
+			var MarkLocation = new L.LatLng(data[i].pov_latitude, data[i].pov_longitude);
+			var marker = new L.marker(MarkLocation, {icon: MarkerIcon}).addTo(map);
+
+			// Définition du contenu du popup
+			txtpop="<b>Lieu sélectionné : </b></br> ";
+			list_artworks="";
+
+			for (var j = 0; j < data[i].artworks.length; j++) {
+
+				list_artworks = list_artworks + data[i].artworks[j].artwork_title + " </br>";
+
+			}
+			txtpop += list_artworks;
+
+			marker.bindPopup(txtpop);
+			marker.pov_ind = i;
+			marker.on("click", function(e) {
+
+				// code pour le clic
+
+				tmp = e.target.pov_ind;
+				list_aw="";
+
+				// Génération de la liste des oeuvres reliées au POV cliqué
+					//Supprimer les oeuvres déjà présentes
+					var oeuvre = document.getElementById('selected_pov_artworks');
+					while (oeuvre.firstChild) {
+						oeuvre.removeChild(oeuvre.firstChild);
+					}
+
+					//créé la nouvelle liste
+				for (var j = 0; j < data[tmp].artworks.length; j++) {
+
+					list_aw = data[tmp].artworks[j].artwork_title;
+
+					var newParagraph = document.createElement('p');
+
+					newParagraph.id = 'id_'+j;
+
+					document.getElementById('selected_pov_artworks').appendChild(newParagraph);
+
+						var newParagraphText = document.createTextNode(list_aw);
+
+						newParagraph.appendChild(newParagraphText);
+
+						//Affichage de la notice liée à l'oeuvre sélectionnée.
+
+						var display_artwork = document.getElementById('id_'+j);
+						display_artwork.artwork_ind = j;
+						display_artwork.pov_ind = tmp ;
+						console.log(display_artwork.artwork_ind);
+
+
+							display_artwork.addEventListener('click', function() {
+								console.log(this.artwork_ind);
+								// suppression de l'oeuvre précédemment affichée
+								var oeuvre_sel = document.getElementById('selected_artwork');
+								while (oeuvre_sel.firstChild) {
+									oeuvre_sel.removeChild(oeuvre_sel.firstChild);
+								}
+							//afficher la dernière oeuvre sélectionnée
+									infos_artwork = data[this.pov_ind].artworks[this.artwork_ind].artwork_title + data[this.pov_ind].artworks[this.artwork_ind].stay_year;
+
+									var newParagraph = document.createElement('p');
+
+									document.getElementById('selected_artwork').appendChild(newParagraph);
+
+									var newParagraphText = document.createTextNode(infos_artwork);
+
+									newParagraph.appendChild(newParagraphText);
+
+							}, false);
+
+					}
+				console.log(list_aw);
+				e.target.setIcon(MarkerIcon2);
+
+			});
+
+			//on groupe tous les marqueurs sur un layer
+			markers_layer.addLayer(marker);
+		}
+		// on ajoute le layer avec tous les markers à la carte
+			},
+			error: function(jqXHR, textStatus, errorThrown)
+			{
+					//if fails
+			}
+	});
+}
 
 
 //callback handler for form submit
 $("#ajaxform").submit(function(e)
 {
     e.preventDefault(); //STOP default action
-    var postData = $(this).serializeArray();
-    var formURL = $(this).attr("action");
-    $.ajax(
-    {
-        url : formURL,
-        type: "POST",
-        data : postData,
-        success:function(data, textStatus, jqXHR)
-        {
-			/*Je boucle sur le tableau */
-
-			for (var i = 0; i < data.length ; i++)
-			{
-
-				var MarkLocation = new L.LatLng(data[i].pov_latitude, data[i].pov_longitude);
-				var marker = new L.marker(MarkLocation, {icon: MarkerIcon}).addTo(map);
-
-				// on rajoute un popup sur le marqueur
-				txtpop="<b>liste des tableaux associés à ce point de vue : </b></br> ";
-				list_artworks="";
-
-				for (var j = 0; j < data[i].artworks.length; j++) {
-
-					list_artworks = list_artworks + data[i].artworks[j].artwork_title + " </br>";
-
-				}
-				txtpop += list_artworks;
-
-
-				marker.bindPopup(txtpop);
-				marker.pov_ind = i;
-				marker.on("click", function(e) {
-
-					// code pour le clic
-
-					tmp = e.target.pov_ind;
-					list_aw="";
-					console.log(tmp);
-					console.log(data[tmp]);
-					console.log(data[tmp].pov_latitude);
-
-					// je génère la liste des oeuvres reliées au pov cliqué
-
-					for (var j = 0; j < data[tmp].artworks.length; j++) {
-
-						list_aw = data[tmp].artworks[j].artwork_title;
-
-						infos_artwork = data[tmp].artworks[j].artwork_title + " </br> " + data[tmp].artworks[j].stay_year;
-
-						var newParagraph = document.createElement('p');
-
-						newParagraph.id = 'id_'+j;
-
-						document.getElementById('selected_pov_artworks').appendChild(newParagraph);
-
-			  			var newParagraphText = document.createTextNode(list_aw);
-
-			  			newParagraph.appendChild(newParagraphText);
-
-			  			//Je crée mon événement sur le click d'un titre d'oeuvre
-
-			  			var display_artwork = document.getElementById('id_'+j);
-
-			  				display_artwork.addEventListener('click', function() {
-
-								var newParagraph = document.createElement('p');
-
-								document.getElementById('selected_artwork').appendChild(newParagraph);
-
-								var newParagraphText = document.createTextNode(infos_artwork);
-
-								newParagraph.appendChild(newParagraphText);
-
-			  				}, false);
-
-						}
-					console.log(list_aw);
-					e.target.setIcon(MarkerIcon2);
-
-				});
-
-				// on ajoute le marqueur à la carte
-				map.addLayer(marker);
-			}
-
-       	},
-        error: function(jqXHR, textStatus, errorThrown)
-        {
-            //if fails
-        }
-    });
-    e.unbind(); //unbind. to stop multiple form submit.
+		sendAjaxForm($("#ajaxform"));
 });
+
+//lancement du formulaire par défaut au chargement de la page
+sendAjaxForm($("#ajaxform"));
